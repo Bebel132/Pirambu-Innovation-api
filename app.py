42dfa81ac0d17e337cce4aac3e8a68aea79bfb6d
emanuel.ferreira <emanuel2005batista@gmail.com>
2025-11-17 15:41:23 -0300

    teste deploy

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, make_response
+from flask_restx import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/teste')
+class hello_world(Resource):
+    def get(self):
+        html = """
+                <!doctype html>
+                <html lang="pt-br">
+                <head><meta charset="utf-8"><title>Hello</title></head>
+                <body>
+                    <h1>Hello, world!</h1>
+                    <p>Deploy OK ✅</p>
+                </body>
+                </html>
+                """
+        resp = make_response(html, 200)
+        resp.headers["Content-Type"] = "text/html; charset=utf-8"
+        return resp
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
